ff7e5aa15adc3a85566c25235d4e29044090210b
Akshay <ap7839426@gmail.com>
2024-09-22 14:46:59 -0400

    Changed the about section of the portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,9 +8,7 @@ const About = () => {
         <article>
           <SectionTitle text='code and coffee' />
           <p className='text-slate-600 mt-8 leading-loose'>
-            A curious individual trying too learn his way through life.
-            Looking for fullstack roles currently MERN stack. 
-            My hobbies include playing reading books, watching anime, playing guitar and working out.
+          Hi, I’m Akshay Patel, a passionate technologist with a background in Chemical Engineering and a current Master’s student in Industrial Engineering at West Virginia University. I have hands-on experience in data science, machine learning, and web development, gained through roles as a Data Science Intern at BlueSpace Labs and a Graduate Research Assistant in pollution prevention. I thrive on solving complex problems and enjoy working on projects that make a positive environmental and societal impact. In my free time, I like to explore cybersecurity and build personal projects,workout, read literature and non fiction, wall climb and play guitar. Feel free to explore my work and connect with me!
           </p>
         </article>
       </div>
